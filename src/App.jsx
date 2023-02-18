917803b0296025ec1b8bdbc7b2d17bdabe690141
AnupamKris <anupamkris13262@gmail.com>
2023-02-18 23:28:19 +0530

    audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { doc } from "firebase/firestore";
 import { useDocument } from "react-firebase-hooks/firestore";
 import { firestore } from "./firebaseConfig";
+import sweet from "./assets/sweet.mp3";
 
 function App() {
   const [count, setCount] = useState(0);
+  const audio = useRef(null);
+
   const [value, loading, error] = useDocument(
     doc(firestore, "dustbins", "bin1")
   );
   useEffect(() => {
     console.log(value);
+    if (value.data()["full"] == true) {
+      audio.current.play();
+    } else {
+      audio.current.pause();
+      audio.current.currentTime = 0;
+    }
   }, [value]);
 
   const getData = () => {
@@ -34,6 +43,7 @@ function App() {
         {!loading && (
           <p>&nbsp;{value.data()["full"] == true ? "Full" : "Empty"}</p>
         )}
+        <audio src={sweet} ref={audio} />
       </div>
     </div>
   );
